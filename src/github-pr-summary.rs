479a2cde9d652f5bd279676f804396aa227e7077
HitanshuPrasad <hitanshuprasad@gmail.com>
2024-07-29 05:19:53 +0530

    Added readme changes

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -125,17 +125,6 @@ async fn handler(event: Result<WebhookEvent, serde_json::Error>) {
     }
     if comment_id == 0u64.into() { return; }
 
-    // Fetch README.md content
-    let readme_content = match octo.repos(&owner, &repo).readme().await {
-        Ok(readme) => {
-            match octo.get::<serde_json::Value, String>(&readme.download_url.unwrap()).await {
-                Ok(content) => content,
-                Err(_) => String::new(),
-            }
-        }
-        Err(_) => String::new(),
-    };
-
     let pulls = octo.pulls(owner.clone(), repo.clone());
     let patch_as_text = pulls.get_patch(pull_number).await.unwrap();
     let mut current_commit = String::new();
@@ -166,8 +155,42 @@ async fn handler(event: Result<WebhookEvent, serde_json::Error>) {
         return;
     }
 
+    async fn fetch_readme(owner: &str, repo: &str) -> Option<String> {
+        let octo = get_octo(&GithubLogin::Default);
+        match octo.repos(owner, repo).readme().await {
+            Ok(readme) => match readme.content {
+                Some(content) => match base64::decode(&content) {
+                    Ok(decoded) => match String::from_utf8(decoded) {
+                        Ok(readme_text) => Some(readme_text),
+                        Err(_) => None,
+                    },
+                    Err(_) => None,
+                },
+                None => None,
+            },
+            Err(_) => None,
+        }
+    }
+    
+
     let chat_id = format!("PR#{pull_number}");
-    let system = &format!("You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled \"{}\". Below is the README.md file content for additional context:\n{}\n\nPlease be as concise as possible while being accurate.", title, readme_content);
+
+    let readme_text = fetch_readme(&owner, &repo).await.unwrap_or_default();
+    let system = if readme_text.is_empty() {
+        format!(
+            "You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled \"{}\". Please be as concise as possible while being accurate.",
+            title
+        )
+    } else {
+        format!(
+            "You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled \"{}\". The following is the README file of the repository for context:\n\n{}\n\nPlease be as concise as possible while being accurate.",
+            title,
+            readme_text
+        )
+    };
+
+
+    // let system = &format!("You are an experienced software developer. You will act as a reviewer for a GitHub Pull Request titled \"{}\". Please be as concise as possible while being accurate.", title);
     let mut lf = LLMServiceFlows::new(&llm_api_endpoint);
     lf.set_api_key(&llm_api_key);
     // lf.set_retry_times(3);
